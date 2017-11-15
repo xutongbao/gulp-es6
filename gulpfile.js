446f8ad452b609c70d0d9827086abc9cbbafc8af
xinghe <2850774095@qq.com>
2017-11-15 13:48:41 +0800

    合并css，替换模版

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,17 @@
 var gulp = require("gulp");
 var babel = require("gulp-babel");
 var watch = require("gulp-watch");
+var htmlreplace = require('gulp-html-replace');
+var fs = require("fs");
+let cleanCSS = require('gulp-clean-css');
+var concat = require('gulp-concat'); 
+var gulpsync = require('gulp-sync')(gulp); 
 
-gulp.task("default", function () {
+gulp.task('default',gulpsync.sync(['babel', 'minify-css', 'mydiv']),function () {  
+  
+});
+
+gulp.task("babel", function () {
     return gulp.src("src/*.js")
         .pipe(babel())
         .pipe(gulp.dest("dist"));
@@ -14,4 +23,37 @@ gulp.task('watch', function() {
         	.pipe(babel())
         	.pipe(gulp.dest("dist"));
 	});
+});
+
+// gulp.task('mycss', function() {
+//   gulp.src('src/index.html')
+//     .pipe(htmlreplace({
+//         'mycss': 'styles.css'
+//     }))
+//     .pipe(gulp.dest('dist'));
+// });
+
+gulp.task('mydiv', function() {
+    let filePath = 'src/index.tpl';
+    var readFile = '';
+    try {
+        fs.accessSync(filePath)
+        readFile = fs.readFileSync(filePath,"utf8");
+        console.log(readFile);
+    } catch (err) {
+        console.log(filePath + ' is not exit ')
+    }
+  	gulp.src('src/index.html')
+    .pipe(htmlreplace({
+        'mydiv': readFile,
+        'mycss': 'styles.css'
+    }))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('minify-css', () => {
+  return gulp.src('src/*.css')
+  	.pipe(concat('styles.css')) 
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('dist'));
 });
