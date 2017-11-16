e2e4bfcfbc987770d71b35a54aebbedb2c5de273
xinghe <2850774095@qq.com>
2017-11-16 11:56:04 +0800

    browserify 包含es6转es5和压缩

--- gulpfile.js
+++ gulpfile.js
@@ -6,9 +6,16 @@ var fs = require("fs");
 let cleanCSS = require('gulp-clean-css');
 var concat = require('gulp-concat'); 
 var gulpsync = require('gulp-sync')(gulp); 
-var browserify = require('gulp-browserify');
+var gulpbrowserify = require('gulp-browserify');
+var browserify = require('browserify');
+var babelify = require('babelify');
+var sourcemaps = require('gulp-sourcemaps');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var standalonify = require('standalonify');
+var uglify = require('gulp-uglify');
 
-gulp.task('default',gulpsync.sync(['babel', 'minify-css', 'mytpl', 'scripts']),function () {  
+gulp.task('default',gulpsync.sync(['babel', 'minify-css', 'mytpl', 'browserify']),function () {  
   
 });
 
@@ -21,20 +28,50 @@ gulp.task("babel", function () {
     return gulp.src(["src/People.js", "src/index.js"])
     	.pipe(concat('index.js')) 
         .pipe(babel())
+        .pipe(uglify({mangle: false, compress: {properties: false}, output: {quote_keys: true}}))
         .pipe(gulp.dest("dist"));
 });
 
-gulp.task('scripts', function() {
-    // Single entry point to browserify 
-    gulp.src('src/main.js')
-        .pipe(browserify({
+gulp.task('gulpbrowserify', function() {
+    gulp.src('src/main.js', { read: false })
+        .pipe(gulpbrowserify({
           insertGlobals : true,
           debug : true
         }))
-        .pipe(babel())
+        //.pipe(babel())
         .pipe(gulp.dest('dist'))
 });
 
+gulp.task('browserify', function() {
+    var b = browserify({
+	  entries: "src/main.js",
+	  debug: true 	//告知browserify在运行同时生成内联sourcemap用于调试
+	});
+	 
+	return b.plugin(standalonify, {  //使打包后的js文件符合UMD规范并指定外部依赖包
+		name: 'FlareJ',
+		deps: {
+			'nornj': 'nj',
+			'react': 'React',
+			'react-dom': 'ReactDOM'
+		}
+		})
+		.transform(babelify, {
+	      presets: [
+	        'es2015'  //转换es6代码
+	      ]
+	    })
+		.bundle()
+		.pipe(source("main.js"))
+		.pipe(buffer())
+		.pipe(uglify({mangle: false, compress: {properties: false}, output: {quote_keys: true}}))
+		.pipe(sourcemaps.init({loadMaps: true}))
+		.pipe(sourcemaps.write("."))
+		//.pipe(uglify()
+		//.pipe(uglify({mangle: false, compress: {properties: false}, output: {quote_keys: true}}))
+		//.pipe(uglify({mangle: false, compress: {properties: false}, output: {quote_keys: true}}))
+		.pipe(gulp.dest("./dist"));
+});
 
 var staticize = {
 	getFileContent : function(tplPathName){
--- src/main.js
+++ src/main.js
@@ -3,3 +3,6 @@ console.log(bar);  // Object
 console.log(bar.variable); // 8
 console.log(bar.sum(1)); // 7
 console.log(bar.square(5)); // 25
+$(function() {
+	console.log('jquery');
+})
